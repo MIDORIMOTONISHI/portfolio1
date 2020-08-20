90d4334d781a925471a298c26cf5b0b587e92ecf
MIDORIMOTONISHI <midoo.nijo@gmail.com>
2020-08-20 14:15:28 +0000

    合計金額まで

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
 module UsersHelper
+  
+  def price_one(size, number)
+    if size == "B0"
+      1600 * number
+    elsif size == "B1"
+      800 * number
+    elsif size == "B2"
+      400 * number
+    elsif size == "A0"
+      1000 * number
+    elsif size == "A1"
+      500 * number
+    elsif size == "A2"
+      300 * number
+    else
+      200 * number
+    end
+  end
 end
